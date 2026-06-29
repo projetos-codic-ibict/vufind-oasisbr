3a770f194f147d8bc3335790b7e6f08f18dbcb72
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2026-06-29 18:13:54 -0300

    feat(theme): migrate header.phtml from Bootstrap 3 to Bootstrap 5
    - Replace navbar-toggle with navbar-toggler and data-bs-* attributes
    - Replace data-toggle with data-bs-toggle on dropdowns
    - Add nav-item and nav-link classes to navigation items
    - Replace isLoggedIn() with getIdentity() for auth check
    - Migrate icons to VuFind 11 icon() helper
    - Migrate language/theme dropdowns to component('menu-button')
    - Add searchbox rendering at footer of nav

--- themes/oasisbr/js/base.js
+++ themes/oasisbr/js/base.js
@@ -34,7 +34,7 @@ if (window.location.hostname === 'oasisbr.ibict.br' || window.location.hostname 
   API_BASE_URL = `https://oasisbr.ibict.br/vufind/api/v1`;
   REMOTE_API_URL = `https://api-oasisbr.ibict.br/`;
 } else {
-  API_BASE_URL = `http://${window.location.host}/vufind/api/v1`;
+  API_BASE_URL = `http://${window.location.host}/oasisbr/api/v1`;
   REMOTE_API_URL = `http://${window.location.host}:3000/api/v1`;
 }
 
